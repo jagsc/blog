e2b9410c37015b1eb28de0582b6466428d3d4671
takanakahiko <takanakahiko@gmail.com>
2019-01-11 15:07:19 +0900

    fix build and test command options

--- _scripts/test.sh
+++ _scripts/test.sh
@@ -1,2 +1,2 @@
-docker run --rm -it -v `pwd`:/srv/jekyll -p 4000:4000 jekyll/jekyll:pages jekyll build -d _site/blog 
-docker run --rm -it -v `pwd`:/srv/jekyll -p 4000:4000 jekyll/jekyll:pages /usr/gem/bin/htmlproofer ./_site --allow-hash-href --check-html --disable-external
+docker run --rm -it -v `pwd`:/srv/jekyll -p 4000:4000 jekyll/jekyll:pages jekyll build
+docker run --rm -it -v `pwd`:/srv/jekyll -p 4000:4000 jekyll/jekyll:pages /usr/gem/bin/htmlproofer ./_site --allow-hash-href --check-html --disable-external --url-swap '^/blog:'
